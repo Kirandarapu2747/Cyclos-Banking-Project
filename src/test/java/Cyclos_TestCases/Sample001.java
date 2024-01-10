d6348a44b1ae49e25000e4bd8b3236d1d1290add
kirandarapu <naveendarapu108@gmail.com>
2024-01-10 11:24:05 +0530

    this is my new Task

---
+++ src/test/java/Cyclos_TestCases/Sample001.java
@@ -0,0 +1,23 @@
+package Cyclos_TestCases;
+
+public class Sample001 {
+	
+	
+  int x=20,y=15;
+  
+  public void sum() {
+	  System.out.println(x+y);
+  }
+  public void sub() {
+	  System.out.println(x-y);
+  }
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
