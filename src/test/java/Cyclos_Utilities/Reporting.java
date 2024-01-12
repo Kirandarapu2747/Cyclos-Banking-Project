846eb33ce705bf6e677d300a3aedfbfa846ba671
dell <dell@DESKTOP-O9G7L1R>
2024-01-12 19:42:47 +0530

    This is my 2nd Task

---
+++ src/test/java/Cyclos_Utilities/Reporting.java
@@ -0,0 +1,60 @@
+package Cyclos_Utilities;
+
+public class Reporting {
+	//Step1:gives the extent report information
+			//gives UI part of the report--Title, doc-name, color...
+			public ExtentSparkReporter reporter;
+			//gives information about the user
+		    public ExtentReports extent;
+		    //gives the status--pass/fail/skip
+		    public ExtentTest test;
+		    public void onStart(ITestContext context) 
+		    {
+		       
+		        //give the report target location
+		        reporter=new ExtentSparkReporter(System.getProperty("user.dir")+"\\Reports\\Report1.html");
+		   
+		        //Title of the report
+		        reporter.config().setDocumentTitle("Automation Testing Report");
+		        
+		        //report name
+		        reporter.config().setReportName("Functional Testing");
+		        
+		        //set the Theme
+		        reporter.config().setTheme(Theme.DARK);
+		       
+		        //user information
+		        extent=new ExtentReports();
+		        extent.attachReporter(reporter);
+		        extent.setSystemInfo("Hostname", "LocalHost");
+		        extent.setSystemInfo("Environment", "QA");
+		        extent.setSystemInfo("Tester", "Prabhavathi");
+		        extent.setSystemInfo("OS", "Windows 10");
+		        extent.setSystemInfo("Browser", "Chrome");
+		    }
+		    //Step2:uses the by default methods
+		    public void onTestSuccess(ITestResult tr) 
+		    {
+		    	//create the new entry to the report
+		        test=extent.createTest(tr.getName());
+		        test.log(Status.PASS,"Testcases is success:"+ tr.getName());
+		    }
+		   
+		    public void onTestFailure(ITestResult tr)
+		    {
+		        test=extent.createTest(tr.getName());
+		        test.log(Status.FAIL, "Testcase is failed: "+ tr.getName());
+		        test.log(Status.FAIL, "exception is : "+ tr.getThrowable());
+		    }
+		   public void onTestSkip(ITestResult tr) 
+		   {
+		       test=extent.createTest(tr.getName());
+		       test.log(Status.SKIP, "Testcase is Skipped: "+ tr.getName());
+		   }
+		   
+		   public void onFinish(ITestContext context) 
+		   {
+		       extent.flush();
+		   }
+
+}
