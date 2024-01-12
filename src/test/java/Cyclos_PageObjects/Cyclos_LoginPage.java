ecd951160f76e8a9fb015ebb54f357a63c1e64eb
ADMIN <ADMIN@192.168.0.204>
2024-01-12 13:29:49 +0530

    This is my 1st task(login)

--- src/test/java/Cyclos_PageObjects/Cyclos_LoginPage.java
+++ src/test/java/Cyclos_PageObjects/Cyclos_LoginPage.java
@@ -1,5 +1,37 @@
 package Cyclos_PageObjects;
 
-public class Cyclos_LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class Cyclos_LoginPage {
+	WebDriver d;
+	public Cyclos_LoginPage(WebDriver d)
+	{
+		this.d=d;
+		PageFactory.initElements(d, this);
+	}
+	@FindBy(xpath="//input[@placeholder='User']")
+	WebElement username;
+	@FindBy(xpath="//input[@placeholder='Password']")
+	WebElement password;
+	@FindBy(xpath="//span[text()='Submit']")
+	WebElement button;
+   @FindBy(xpath="//a[@id='forgotPasswordLink']")
+   WebElement forgetpwd;
+   @FindBy(xpath="//a[@id='registerLink']")
+   WebElement reglink;
+   public void setUsername(String name)
+   {
+	   username.sendKeys(name); 
+   }
+   public void setPassword(String pwd)
+   {
+	   password.sendKeys(pwd); 
+   }
+   public void clickLogin()
+   {
+	   button.click();
+   }
 }
--- src/test/java/Cyclos_TestCases/Cyclos_BaseClass.java
+++ src/test/java/Cyclos_TestCases/Cyclos_BaseClass.java
@@ -1,5 +1,36 @@
 package Cyclos_TestCases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Cyclos_PageObjects.Cyclos_LoginPage;
+
 public class Cyclos_BaseClass {
+	WebDriver d;
+	Cyclos_LoginPage clp;
+	@BeforeClass
+	void setUP() throws Exception
+	{
+		d=new ChromeDriver();
+		d.get("https://demo.cyclos.org/ui/login");
+		d.manage().window().maximize();
+		Thread.sleep(3000);
+	}
+	@Test
+	void testLogin()
+	{
+		clp=new Cyclos_LoginPage(d);
+		clp.setUsername("demo");
+		clp.setPassword("1234");
+		clp.clickLogin();
+	}
+	@AfterClass
+	void teanDown()
+	{
+		d.close();
+	}
 
 }
