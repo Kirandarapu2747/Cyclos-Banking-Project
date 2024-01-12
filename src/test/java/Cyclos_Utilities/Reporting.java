d2313c04f96f4be911b564eadb61867ad5274714
dell <dell@DESKTOP-O9G7L1R>
2024-01-12 20:10:58 +0530

    This is my 2nd task

--- src/test/java/Cyclos_Utilities/Reporting.java
+++ src/test/java/Cyclos_Utilities/Reporting.java
@@ -1,6 +1,14 @@
 package Cyclos_Utilities;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class Reporting implements ITestListener{
 	//Step1:gives the extent report information
