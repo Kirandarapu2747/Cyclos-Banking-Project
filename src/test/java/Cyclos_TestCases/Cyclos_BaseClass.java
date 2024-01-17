8c44a7fe558bf6ca7e0e105470918b6884a07637
dell <dell@DESKTOP-O9G7L1R>
2024-01-17 09:38:34 +0530

    This is my 2nd Task

--- src/test/java/Cyclos_TestCases/Cyclos_BaseClass.java
+++ src/test/java/Cyclos_TestCases/Cyclos_BaseClass.java
@@ -6,8 +6,12 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
 import Cyclos_Utilities.Cyclos_ReadConfig;
 
 public class Cyclos_BaseClass {
@@ -18,9 +22,21 @@ public class Cyclos_BaseClass {
 	public String Username=rc.getUsername();
 	public String Password=rc.getPassword();
 	@BeforeClass
-	public void OpenApplication()
+	@Parameters("browser")
+	public void OpenApplication(String br)
 	{
-		driver=new ChromeDriver();
+		if(br.equals("chrome"))
+		{
+			driver=new ChromeDriver();
+		}
+		else if(br.equals("edge"))
+		{
+			driver=new EdgeDriver();
+		}
+		else if(br.equals("firefox"))
+		{
+			driver=new FirefoxDriver();
+		}
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get(URL);
 		driver.manage().window().maximize();
